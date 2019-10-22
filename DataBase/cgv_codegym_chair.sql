6827c35494c3593265937d747d4db171ecadc774
toantran134 <toantran134@gmail.com>
2019-10-22 20:52:01 +0700

    toan up file

---
+++ DataBase/cgv_codegym_chair.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cgv_codegym
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `chair`
+--
+
+DROP TABLE IF EXISTS `chair`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chair` (
+  `id_chair` int(11) NOT NULL AUTO_INCREMENT,
+  `column_chair` int(11) NOT NULL,
+  `row_chair` int(11) NOT NULL,
+  `room` varchar(255) NOT NULL,
+  `status_chair` int(11) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `price_type_chair` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `choise_status` bit(1) DEFAULT NULL,
+  `chair_detail` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_chair`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chair`
+--
+
+LOCK TABLES `chair` WRITE;
+/*!40000 ALTER TABLE `chair` DISABLE KEYS */;
+INSERT INTO `chair` VALUES (1,1,1,'1',1,'A1',0,'A1',_binary '\0',1),(2,2,1,'2',1,'A2',0,'A2',_binary '\0',1),(3,3,1,'2',1,'A3',0,'A3',_binary '\0',1),(4,4,1,'2',1,'A4',0,'A4',_binary '\0',1),(5,5,1,'2',1,'A5',0,'A5',_binary '\0',1),(6,6,1,'1',1,'A6',0,'A6',_binary '\0',1),(7,1,2,'1',1,'B1',0,'B1',_binary '\0',1),(8,2,2,'1',1,'B2',0,'B2',_binary '\0',1),(9,3,2,'1',1,'B3',0,'B3',_binary '\0',1),(10,4,2,'1',1,'B4',0,'B4',_binary '\0',1),(11,5,2,'1',1,'B5',0,'B5',_binary '\0',1),(12,6,2,'1',1,'B6',0,'B6',_binary '\0',1),(13,1,3,'1',1,'C1',0,'C1',_binary '\0',1),(14,2,3,'1',1,'C2',0,'C2',_binary '\0',1),(15,3,3,'1',1,'C3',0,'C3',_binary '\0',1),(16,4,3,'1',1,'C4',0,'C4',_binary '\0',1),(17,5,3,'1',1,'C5',0,'C5',_binary '\0',1),(18,6,3,'1',1,'C6',0,'C6',_binary '\0',1),(19,1,4,'1',1,'D1',0,'D1',_binary '\0',1),(20,2,4,'1',1,'D2',0,'D2',_binary '\0',1),(21,3,4,'1',1,'D3',0,'D3',_binary '\0',1),(22,4,4,'1',1,'D4',0,'D4',_binary '\0',1),(23,5,4,'1',1,'D5',0,'D5',_binary '\0',1),(24,6,4,'1',1,'D6',0,'D6',_binary '\0',1);
+/*!40000 ALTER TABLE `chair` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-22 20:24:26
